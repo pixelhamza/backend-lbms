f9622e3e10f0d7e20798409d4d0bc502c2911549
Nashit Hamza <nashit@Nashits-MacBook-Air.local>
2025-09-26 00:53:05 +0530

    final

--- lbms_project/settings.py
+++ lbms_project/settings.py
@@ -122,7 +122,8 @@ REST_FRAMEWORK = {
 }
 CORS_ALLOWED_ORIGINS = [
     "http://localhost:5173", 
-    "http://127.0.0.1:5173", 
+    "http://127.0.0.1:5173",
+    "backend-lbms-1.onrender.com", 
 ]
 
 
