a4472a7f9cfaa12f783500d2b35a3d251e1bbd36
Nashit Hamza <nashit@Nashits-MacBook-Air.local>
2025-09-26 09:40:49 +0530

    final

--- lbms_project/settings.py
+++ lbms_project/settings.py
@@ -50,6 +50,7 @@ INSTALLED_APPS = [
 ]
 
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -57,7 +58,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    'corsheaders.middleware.CorsMiddleware',
+    
 ]
 
 ROOT_URLCONF = 'lbms_project.urls'
@@ -123,7 +124,7 @@ REST_FRAMEWORK = {
 CORS_ALLOWED_ORIGINS = [
     "http://localhost:5173", 
     "http://127.0.0.1:5173",
-    "https://backend-lbms-3.onrender.com",
+    "https://lbms-frontend-1.onrender.com",
 ]
 
 
